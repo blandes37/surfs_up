1b20ff6556ac105c59bcf4e1725500244ca34083
Ben <68405490+blandes37@users.noreply.github.com>
2020-09-15 20:56:18 -0400

    try again

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-Measurement = Base.classes.measurement
+
 Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 session = Session(engine)
 
@@ -31,3 +32,10 @@ def welcom():
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
