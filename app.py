1234fa4334e308d2df491f2df5508d01d9e408d4
Ben <68405490+blandes37@users.noreply.github.com>
2020-09-15 20:20:15 -0400

    Trouble with \n

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jasonify
+from flask import Flask, jsonify
 
-engine = create_engine("sqlite://hawaii.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
-Base.prepare(engine, reflect=true)
+Base.prepare(engine, reflect=True)
 
-Measurement = Base.classes.Measurement
+Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 session = Session(engine)
