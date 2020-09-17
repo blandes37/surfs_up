795ef6a0548bf7e568b22761e9d5c79605c9833a
Ben <68405490+blandes37@users.noreply.github.com>
2020-09-17 06:53:36 -0400

    ok

--- app.py
+++ app.py
@@ -17,7 +17,18 @@ Station = Base.classes.station
 session = Session(engine)
 
 app = Flask(__name__)
+@app.route("/")
 
+def welcome():
+    test = (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
+    return test
 
 @app.route("/")
 def precipitation():
